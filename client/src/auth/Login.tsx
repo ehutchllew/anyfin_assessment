a553222333e816766867540e323b7b3f14bee743
Evan <ehutch7@gmail.com>
2021-02-16 16:36:56 -0500

    Adding base service.

---
+++ client/src/auth/Login.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const Login = () => {
+
+    function onSignInPressed(e: any){
+        e.preventDefault();
+    }
+
+    return(
+        <main className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+        <section>
+            <h3 className="font-bold text-2xl">Välkommen</h3>
+            <p className="text-gray-600 pt-2">Sign in to your account.</p>
+        </section>
+
+        <section className="mt-10">
+                <div className="mb-6 pt-3 rounded bg-gray-200">
+                    <label className="block text-gray-700 text-sm font-bold mb-2 ml-3" htmlFor="username">Username</label>
+                    <input type="text" id="username" className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"/>
+                </div>
+                <div className="mb-6 pt-3 rounded bg-gray-200">
+                    <label className="block text-gray-700 text-sm font-bold mb-2 ml-3" htmlFor="password">Password</label>
+                    <input type="password" id="password" className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" />
+                </div>
+                <div className="flex justify-end">
+                    <a href="#" className="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">Forgot your password?</a>
+                </div>
+
+                    <button className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" onClick={onSignInPressed}>Sign In</button>
+                
+        </section>
+    </main>
+    )
+}
---
+++ client/src/services/baseService.ts
@@ -0,0 +1,46 @@
+export async function baseService(req: any, options: any) {
+    const DEFAULT_HEADERS = {
+        "Content-Type": "application/json",
+    };
+    const { body, headers = DEFAULT_HEADERS, method = "GET", path = "" } = req;
+    const baseUrl = "http://localhost:3001/";
+    const url = new URL(baseUrl + path);
+    const fetchInit: RequestInit = {
+        method,
+        headers,
+        credentials: "include",
+    };
+
+    if (body) {
+        fetchInit.body = JSON.stringify(body);
+    }
+
+    if (options) {
+        const searchParams = new URLSearchParams({});
+        if (options.filter) {
+            const hashedFilter = btoa(JSON.stringify(options.filter));
+            searchParams.set("filter", hashedFilter);
+        }
+
+        if (options.sort) {
+            const hashedSort = btoa(JSON.stringify(options.sort));
+            searchParams.set("sort", hashedSort);
+        }
+
+        url.search = searchParams.toString();
+    }
+
+    const rawResponse = await fetch(url.toString(), fetchInit);
+
+    const parsedResponse = await rawResponse.json();
+
+    if (!rawResponse.ok) {
+        throw parsedResponse;
+    }
+    /**
+     * TODO: Consider having a global error handler here that checks the
+     * `rawResponse` status and throws. The global error handler can be
+     * a modal that renders globally and is shown based off redux state.
+     */
+    return parsedResponse;
+}
